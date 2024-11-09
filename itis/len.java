9dee7a9c23e6ea0fc3509f25c9ad6a77c8d3a17e
Gulnara710 <gulnaranazipova70262@gmail.com>
2024-11-09 12:21:57 +0300

    изменен вид переменной

--- itis/len.java
+++ itis/len.java
@@ -3,8 +3,8 @@ public class Program {
     public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Задание 2. Введите число");
-		long f = 1;
-		for (long x = sc.nextLong(); x >= 1; x--){
+		int f = 1;
+		for (int x = sc.nextInt(); x >= 1; x--){
 			f = f * x;
 		}
 		System.out.println(f);
